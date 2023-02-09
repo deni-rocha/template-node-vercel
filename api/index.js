25b2069cd017668334657c9245a2db121ee02ca5
Denilson Rocha <denilsonrocha_@outlook.com>
2023-02-09 13:41:28 -0300

    base da aplicação com express

---
+++ api/index.js
@@ -0,0 +1,19 @@
+import express, { Router } from "express";
+import cors from "cors";
+
+//configurações
+const app = express();
+const router = Router();
+
+app.use(cors());
+app.use(express.json());
+app.use(router);
+
+// rotas
+router.get("/api", (req, res) => {
+  res.send({
+    msg: "parabéns sua API está em funcionamento",
+  });
+});
+
+app.listen(3000, () => console.log("está rodando na porta 3000"));
